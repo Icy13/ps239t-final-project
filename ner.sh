ed3bafd0738d3f836f2125c488888788e49cf0d5
Konrad Posch <konrad.posch@gmail.com>
2015-12-07 17:06:16 -0800

    reg.gov code which runs all the way, with some inconsistencies but no errors, next I will clean the debug codes out

--- ner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-scriptdir=`dirname $0`
-
-cd "D:/Konrads Word f/_Classes f/Dropbox/_Berkeley/_Fall 2015/PS 239T - 
-Computational Methods\stanford-ner-2015-04-20
-
-java -mx1500m -cp stanford-ner.jar edu.stanford.nlp.ie.NERServer -loadClassifier classifiers/english.all.3class.distsim.crf.ser.gz -port 8080 -outputFormat inlineXML
